66c6119565477a32dc17bd68f70e25832f8b3de2
lliurex <juanma1980@gmail.com>
2023-03-28 07:50:45 +0200

    only enable/disable

--- src/profiles.d/lliurex-up.py
+++
@@ -1,4 +0,0 @@
-profile /usr/share/lliurex-up/lliurex-up.py {
-  audit /usr/sbin/lliurex-up ix,
-}
-
--- src/profiles.d/zero-center.py
+++
@@ -1,4 +0,0 @@
-profile /usr/share/zero-center/zero-center.py flags=(complain) {
-  /usr/bin/zero-center ix,
-  }
-
